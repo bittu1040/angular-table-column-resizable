413ef0db9d48d59c5010a8aa21ecdb8056c05c3a
Bittu Kumar <32195733+bittu1040@users.noreply.github.com>
2023-09-08 02:25:55 +0530

    Update resizable.directive.ts

--- src/app/directives/resizable.directive.ts
+++ src/app/directives/resizable.directive.ts
@@ -40,3 +40,46 @@ export class ResizableDirective {
 
 
 }
+
+
+
+
+
+// You can use this also, different way 
+/*
+import { Directive, ElementRef, HostListener } from '@angular/core';
+
+@Directive({
+  selector: '[appResizable]'
+})
+export class ResizableDirective {
+
+  private startX: number | any;
+  private startWidth: number | any;
+
+  constructor(private el: ElementRef) {}
+
+  @HostListener('mousedown', ['$event'])
+  onMouseDown(event: MouseEvent) {
+    this.startX = event.pageX;
+    this.startWidth = this.el.nativeElement.offsetWidth;
+    event.preventDefault();
+    event.stopPropagation();
+  }
+
+  @HostListener('document:mousemove', ['$event'])
+  onMouseMove(event: MouseEvent) {
+    if (this.startX) {
+      const diff = event.pageX - this.startX;
+      this.el.nativeElement.style.width = this.startWidth + diff + 'px';
+    }
+  }
+
+  @HostListener('document:mouseup')
+  onMouseUp() {
+    this.startX = undefined;
+    this.startWidth = undefined;
+  }
+
+}
+*/
